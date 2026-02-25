12c427daa6511501679c50e37d02905703e72f88
manchananavya-prog <manchananavya@gmail.com>
2026-02-25 16:15:09 +0530

    🚀 Day 26 - index.html - 4:15:04 PM

---
+++ Day-26/script.js
@@ -0,0 +1,4 @@
+document.getElementById("checkBtn").addEventListener("click", function () {
+    document.getElementById("message").innerText =
+        "MongoDB Project Setup Completed Successfully!";
+});
